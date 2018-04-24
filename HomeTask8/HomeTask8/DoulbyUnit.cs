4c0e53c0630bd8d96e0eb49fd13adf77936031dd
Alex <proskuraaleksandr93@gmail.com>
2018-04-24 23:18:49 +0300

    created doublyLinkedList

---
+++ HomeTask8/HomeTask8/DoulbyUnit.cs
@@ -0,0 +1,14 @@
+﻿namespace HomeTask8
+{
+    internal class DoulbyUnit<T>
+    {
+        public DoulbyUnit(T data)
+        {
+            Data = data;
+        }
+
+        public T Data { get; set; }
+        public DoulbyUnit<T> Previous { get; set; }
+        public DoulbyUnit<T> Next { get; set; }
+    }
+}
